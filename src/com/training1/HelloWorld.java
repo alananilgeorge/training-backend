e19a98bf5d093a6e2d143ca9f394b2535e23af74
alananil.george <alananil.george@gmail.com>
2022-08-08 00:22:46 +0530

    calculator pgm added

--- src/com/training1/HelloWorld.java
+++ src/com/training1/HelloWorld.java
@@ -1,11 +1,49 @@
 package com.training1;
+import java.util.Scanner;
+
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		for(int i =0;i<5;i++) {
-			System.out.println("Hello World");
+		System.out.println("Enter input numbers");
+		Scanner sc = new Scanner(System.in);
+		int var1 = sc.nextInt();
+		int var2 = sc.nextInt();
+		System.out.println("Enter operation (+,-,*,/,%)");
+		char op = sc.next().charAt(0);
+		System.out.println("Answer is: ");
+		if(op=='+') {
+			System.out.println(var1+var2);
 		}
-	}
-
+		else if(op=='-') {
+			if(var1>var2) {
+				System.out.println(var1-var2);
+			}
+			else {
+				System.out.println(var2-var1);
+			}
+		}
+		else if(op=='*') {
+				System.out.println("test");
+				System.out.println(var1*var2);
+		}
+		else if(op=='/') {
+				if(var1>var2) {
+						System.out.println(var1/var2);
+				}
+				else {
+							System.out.println(var2/var1);
+						}
+		}
+		else if(op=='%'){
+				if(var1>var2) {
+						System.out.println(var1%var2);
+				}
+		else {
+							System.out.println(var2%var1);
+			}
+				}
+				
+		}
 }
+
 
